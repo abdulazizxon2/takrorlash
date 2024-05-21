8a8276c48b8928da13c58da5cff95fad64c49bbb
abdulazizxon2 <boydadayevabbos@gmail.com>
2024-05-21 17:05:11 +0500

    takrorlash

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Main from "./main/Main";
+import "./App.css";
+// import PageIndex from "./ReducerDars/page/PageIndex";
+function App() {
+  return (
+    <div className="App container">
+      {/* <PageIndex /> */}
+      <Main />
+    </div>
+  );
+}
+export default App;
